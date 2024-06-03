2812fbc4ae10f6e48d32a7a2f6fd53984a465e35
srirangamuc <uc3932@gmail.com>
2024-06-03 22:49:49 +0530

    Updates on package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/blog');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/blogs', usersRouter);
+app.use('/blogs', blogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(5000,()=>{
+    console.log("Server running on 5000");
+})
 module.exports = app;
